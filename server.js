48b5051f1d29a8e04ede10eeb80184feb864b181
Ian Avery <ianedavery@gmail.com>
2018-02-25 18:06:04 -0700

    added yml

--- server.js
+++ server.js
@@ -38,10 +38,6 @@ passport.use(jwtStrategy);
 
 const jwtAuth = passport.authenticate('jwt', {session: false});
 
-app.get('/api', (req, res) => {
-   res.json({ok: true});
- });
-
 app.use('/api/users/', usersRouter);
 app.use('/api/auth/', authRouter);
 app.use('/api/recipies/', jwtAuth, recipieRouter);
